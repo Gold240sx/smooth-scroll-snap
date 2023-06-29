b3e55b2a419a6318bcd6db9bb5940f86d911e993
Gold240sx <240designworks@gmail.com>
2023-06-29 00:59:37 -0500

    1. added minify output

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { terser } from "rollup-plugin-terser"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react(),
+		terser({
+			// Terser options
+			format: {
+				comments: false,
+			},
+		}),
+	],
 })
