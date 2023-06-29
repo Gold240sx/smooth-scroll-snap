a100b5b44094906060f33eb5467cf845600cb5b9
Gold240sx <240designworks@gmail.com>
2023-06-29 16:25:42 -0500

    3. Section 3 vercel update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { Navbar } from "./components/Navbar"
 import { Section } from "./components/Section"
-import { color } from "framer-motion"
 
 //Components
 
